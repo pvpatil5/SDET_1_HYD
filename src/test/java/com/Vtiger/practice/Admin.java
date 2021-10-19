1b99da0872f02a50770a05190ba84a7fc9608aa7
pvpatil5 <pvpatil123@yahoo.com>
2021-10-19 12:24:53 +0530

    New Class added admin

---
+++ src/test/java/com/Vtiger/practice/Admin.java
@@ -0,0 +1,11 @@
+package com.Vtiger.practice;
+
+public class Admin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+	System.out.println("New admin Added");	
+	
+	}
+
+}
