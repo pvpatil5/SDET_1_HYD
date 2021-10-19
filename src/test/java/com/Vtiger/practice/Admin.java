93ffc4b06ded28ae1a68a63ca1cce8abf4d2f954
12345Mukesh <cmukesh391@gmail.com>
2021-10-19 12:33:04 +0530

    new

--- src/test/java/com/Vtiger/practice/Admin.java
+++ src/test/java/com/Vtiger/practice/Admin.java
@@ -2,10 +2,12 @@ package com.Vtiger.practice;
 
 public class Admin {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub	
 	System.out.println("New admin Added");	
 	
+	System.out.println("New line added by mukesh");
 	}
 
 }
