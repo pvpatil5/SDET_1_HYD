24317ddc394773fe41be5bafe92af746ac1e0d90
pvpatil5 <pvpatil123@yahoo.com>
2021-10-19 12:29:49 +0530

    New line added by pavan

--- src/test/java/com/Vtiger/practice/Admin.java
+++ src/test/java/com/Vtiger/practice/Admin.java
@@ -4,8 +4,10 @@ public class Admin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub	
-	System.out.println("New admin Added");	
-	
+		System.out.println("New admin Added");	
+
+		System.out.println("New line added by Pavan");
+
 	}
 
 }
